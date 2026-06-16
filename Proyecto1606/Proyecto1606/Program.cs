5f951982635545b1a1f2ce9070cedf50cf34cc7d
fran <frangarcia2707@gmail.com>
2026-06-16 09:43:31 +0200

    cambios menores

--- Proyecto1606/Proyecto1606/Program.cs
+++ Proyecto1606/Proyecto1606/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+
+        static void Ejercicio1()
+        {
+
+        }
+
+        static void Ejercicio2()
+        {
+
+        }
+
+        static void Ejercicio3()
+        {
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ejercicio1();
+            //Ejercicio2();
+            //Ejercicio3();
         }
     }
 }
