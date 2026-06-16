d329944612cf99eba1590b71808798bb906765b9
fran <frangarcia2707@gmail.com>
2026-06-16 09:42:41 +0200

    Plantillas

---
+++ Proyecto1606/Proyecto1606/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto1606
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
