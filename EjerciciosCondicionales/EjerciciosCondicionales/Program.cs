a64b13e791e84cf9d940db5bfbbafddc2e35ea44
fran <frangarcia2707@gmail.com>
2026-06-23 11:38:53 +0200

    Esqueleto Ejercicios Condicionales

---
+++ EjerciciosCondicionales/EjerciciosCondicionales/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace EjerciciosCondicionales
+{
+    internal class Program
+    {
+
+        static void Ejercicio1()
+        {
+
+        }
+        static void Ejercicio2()
+        {
+
+        }
+        static void Ejercicio3()
+        {
+
+        }
+        static void Ejercicio4()
+        {
+
+        }
+        static void Ejercicio5()
+        {
+
+        }
+        static void Ejercicio6()
+        {
+
+        }
+        static void Ejercicio7()
+        {
+
+        }
+        static void Ejercicio8()
+        {
+
+        }
+        static void Ejercicio9()
+        {
+
+        }
+        static void Ejercicio10()
+        {
+
+        }
+        static void Ejercicio11()
+        {
+
+        }
+        static void Ejercicio12()
+        {
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Ejercicio1();
+            //Ejercicio2();
+            //Ejercicio3();
+            //Ejercicio4();
+            //Ejercicio5();
+            //Ejercicio6();
+            //Ejercicio7();
+            //Ejercicio8();
+            //Ejercicio9();
+            //Ejercicio10();
+            //Ejercicio11();
+            //Ejercicio12();
+        }
+    }
+}
