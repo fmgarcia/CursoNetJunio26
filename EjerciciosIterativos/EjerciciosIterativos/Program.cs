63f3b9b6f5dcbfacd5b89d4fce91be30cae0b241
fran <frangarcia2707@gmail.com>
2026-06-25 13:26:55 +0200

    Ejercicio 14

--- EjerciciosIterativos/EjerciciosIterativos/Program.cs
+++ EjerciciosIterativos/EjerciciosIterativos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Pipelines;
+using System.Runtime.Intrinsics.X86;
 
 namespace EjerciciosIterativos
 {
@@ -59,18 +60,54 @@ namespace EjerciciosIterativos
                 Console.Write("*");
             }
         }
-
+        // Muestra los números del 1 al 30 menos los divisibles entre 3 (resto da cero).
         static void Ejercicio5()
         {
-
+            for (int i = 1; i <= 30; i++)
+                if (i % 3 != 0)
+                    Console.WriteLine(i);
         }
+        // Muestra de 5 en 5, los números del 0 al 100.
         static void Ejercicio6()
         {
-
+            for (int i = 0; i <= 100; i += 5)
+                Console.WriteLine(i);
         }
+        //Pregunta al usuario un número y muestra del 1 hasta ese número, en una misma línea,
+        //separados por comas(debes comprobar cuando es el último y no poner una coma en
+        //ese caso).
+        //Dime un número: 7
+        //1,2,3,4,5,6,7
         static void Ejercicio7()
         {
-
+            Console.WriteLine("Introduce un número");
+            int num = int.Parse(Console.ReadLine()!);
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write(i + (i != num ? "," : ""));
+            }
+        }
+        static void Ejercicio7b()
+        {
+            Console.WriteLine("Introduce un número");
+            int num = int.Parse(Console.ReadLine()!);
+            for (int i = 1; i <= num; i++)
+            {
+                if (i != num)
+                {
+                    Console.Write(i + ",");
+                }
+                else  // Si es el último número, no ponemos la coma (i == num)
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+        static void Ejercicio7c()
+        {
+            Console.WriteLine("Introduce un número");
+            int num = int.Parse(Console.ReadLine()!);
+            Console.WriteLine(string.Join(",", Enumerable.Range(1, num)));  // Utilizamos LINQ para generar la secuencia de números y luego los unimos con comas
         }
         static void Ejercicio8()
         {
@@ -139,9 +176,23 @@ namespace EjerciciosIterativos
                 Console.WriteLine();
             }
         }
+        //Haz lo mismo pero con una escalera de subida
+        //(para generar los huecos debes escribir el carácter de espacio).
+        //Dime un número: 4
+        //   *
+        //  **
+        // ***
+        //****
         static void Ejercicio14()
         {
-
+            Console.WriteLine("Da un número entero para la altura: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                    Console.Write(j <= num - i ? " " : "*");
+                Console.WriteLine();
+            }
         }
         static void Ejercicio15()
         {
@@ -183,8 +234,8 @@ namespace EjerciciosIterativos
             //Ejercicio10();
             //Ejercicio11();
             //Ejercicio12();
-            Ejercicio13();
-            //Ejercicio14();
+            //Ejercicio13();
+            Ejercicio14();
             //Ejercicio15();
             //Ejercicio16();
             //Ejercicio17();
